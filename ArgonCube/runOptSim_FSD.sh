b8551a18cddcdebea126dadb4b74a4ba17e950bb
liviocali <liviocalivers@hotmail.com>
2022-12-20 14:49:43 +0100

    Livio: Add FSD geometries for optical simulation

---
+++ ArgonCube/runOptSim_FSD.sh
@@ -0,0 +1,2 @@
+gegede-cli ../duneggd/Config/ArgonCube/OptSim_FSD.cfg ../duneggd/Config/ArgonCube/DETENCLOSURE_OptSim.cfg ../duneggd/Config/WORLDggd.cfg -w World -o OptSim_FSD.gdml
+root -l 'materialDisplay.C("OptSim_FSD.gdml")'
